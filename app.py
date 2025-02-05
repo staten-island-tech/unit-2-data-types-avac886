59a644e14f8b87e2c554aac6a9b8482735d0bd75
avac886 <avac886@gmail.com>
2025-02-05 09:25:35 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+""" #Integers and Floats
+x = 3
+y = float(3)
+print(x,y)
+ """
+#Tip Calculator
+bill = input("How much was the bill?")
+print(bill)
+
+tip = input("How much was the tip?")
+print(tip)
+
