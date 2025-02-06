c27ac038c9f2ab4d9e6f384296a01cfea85f1849
avac886 <avac886@gmail.com>
2025-02-06 09:24:54 -0500

    unit 2

--- app.py
+++ app.py
@@ -3,10 +3,30 @@ x = 3
 y = float(3)
 print(x,y)
  """
+
 #Tip Calculator
-bill = input("How much was the bill?")
-print(bill)
+bill = 3
+tip = 2
+total = 1
 
-tip = input("How much was the tip?")
-print(tip)
+""" #Lists
+values = [1,2.23,5,7,2,30,15]
+print(values)
+for i in values:
+    print(i) """
 
+""" #What if we just want one single, specific value?
+values = [1,2.23,5,7,2,30,15,4]
+print(values[0])
+print(values[7]) """
+
+""" #Strings
+"test"
+["t","e","s","t"]
+
+#String Methods
+x = "this is a thing"
+y= x.split()
+z = y[1]
+print(y)
+print(z) """
