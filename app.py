3ed19a0e4f98d87e87563fa4b7f52b06d3a87808
avac886 <avac886@gmail.com>
2025-02-06 21:09:39 -0500

    unit 2

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ y = float(3)
 print(x,y)
  """
 
+'''''
 #Tip Calculator
 bill = input("Enter bill amount:")
 tip = input("Enter tip amount:")
@@ -13,6 +14,7 @@ tip = int(tip)
 
 total = bill + tip
 print(f"The bill is ${bill}. The tip is ${tip}. The total is ${total}")
+'''''
 
 """ #Lists
 values = [1,2.23,5,7,2,30,15]
@@ -25,14 +27,27 @@ values = [1,2.23,5,7,2,30,15,4]
 print(values[0])
 print(values[7]) """
 
-""" #Strings
+'''''
+#Strings
 "test"
 ["t","e","s","t"]
+'''''
 
+'''''
 #String Methods
 x = "this is a thing"
 y= x.split()
 z = y[1]
 print(y)
-print(z) """
+print(z)
+'''''
 
+#Challenge 1
+sentence = input("Enter a sentence:")
+def WordCount():
+    x = sentence.split()
+    print(x)
+    print(len(x))
+WordCount()
+
+#Challenge 2
