09cf23b0363a2c1ab922f9b2a52509348f9aa418
avac886 <avac886@gmail.com>
2025-02-27 09:04:39 -0500

    unit 2

--- app.py
+++ app.py
@@ -148,16 +148,13 @@ def factors():
 factors()
 '''''
 
-
 #Challenge 4
-def gcf():
-    a = int(input("Smaller number"))
-    b = int(input("Bigger number"))
+def gcf(a, b):
     for i in range(1, a+1):
         if a % i == 0 and b % i == 0:
             print(i)
 
-gcf()
+gcf(10, 20)
 
 """ def login(password):
     #if statement evaluates to true d next line
